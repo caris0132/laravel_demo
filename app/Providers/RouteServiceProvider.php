5a06f551f9cb8a0efb719beed3ab2f4fd89bf0ed
caris0132 <nguyenductri1h1@gmail.com>
2020-03-23 16:48:30 +0700

    add laravel ui vue

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapAdminRoutes();
+
         //
     }
 
@@ -76,5 +78,10 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    protected function mapAdminRoutes()
+    {
+        Route::prefix('admin')->namespace($this->namespace . '\Admin')->group(base_path('routes/admin.php'));
     }
 }
